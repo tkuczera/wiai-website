ee1175627c48d497d4d6ed7a2d20b0a8c39f4b1f
tkuczera <50948817+tkuczera@users.noreply.github.com>
2019-05-23 12:47:41 +0200

    Create skrypt.js

---
+++ prosty_skrypt/res/skrypt.js
@@ -0,0 +1,5 @@
+function zN2() {
+	var x = document.getElementById("Paragraf2");
+	x.style.color = "blue";
+	Paragraf2.innerHTML = "Informacja z funkcji zdefiniowanej w skrypcie zewnętrznym...";
+	}
